96cca3efac565afb9bb6f0a9a445e72184a6bb9b
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-11-14 23:07:45 +0700

    fix section farmer care

--- src/components/FarmerCare/FarmerCard.jsx
+++ src/components/FarmerCare/FarmerCard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function FarmerCard({ icon, title, desc }) {
   return (
-    <div className="flex items-start bg-gray-50 p-4 rounded-lg shadow-md gap-3">
+    <div className="flex items-center items-start bg-gray-300 p-4 rounded-lg shadow-md gap-3">
       <img className="w-1/5" src={icon} />
-      <div>
-        <h3 className="text-yellow-600 font-bold text-lg"> {title} </h3>
-        <p className="text-gray-700">{desc}</p>
+      <div className="mt-3">
+        <h3 className="text-green-500 font-bold text-lg"> {title} </h3>
+        <p className="text-neutral-500">{desc}</p>
       </div>
     </div>
   );
